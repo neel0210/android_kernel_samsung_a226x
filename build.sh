03f7c85283aebf260b50d6594e3674db58979325
Neel <neeljadhavforcoc@gmail.com>
2024-08-03 19:01:15 -0400

    update: build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-export CROSS_COMPILE=$(pwd)/toolchain/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-androidkernel-
-export CC=$(pwd)/toolchain/clang/host/linux-x86/clang-r383902/bin/clang
+PROTON_PATH="/home/itachi/proton"
+
+
+export CROSS_COMPILE="/home/itachi/49/bin/aarch64-linux-android-"
+export CC="/home/itachi/proton/bin/clang"
 export CLANG_TRIPLE=aarch64-linux-gnu-
 export ARCH=arm64
 export ANDROID_MAJOR_VERSION=r
